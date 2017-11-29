9b317771ab59c68e9cf8d9de99bd94a9915a8d60
刘刘 <liuliu@mofanghr.com>
2017-11-29 16:38:25 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+<div id="app">
+	<el-container>
+		<el-header>
+            <div class="logo-wrap">
+                <span class="image">
+                    <img :src="logo" height="43" />
+                </span>
+                <span class="logo-text">
+                    魔方微猎
+                </span>
+            </div>
+        </el-header>
+		<el-container>
+			<el-aside width="200px">
+                <AsidePanel />
+            </el-aside>
+			<el-main>
+                <div class="subTabBar">
+                    <SubTabPanel />
+                </div>
+                <div>
+	                <router-view/>
+                </div>
+			</el-main>
+		</el-container>
+	</el-container>
+</div>
+</template>
+
+<script>
+import logo from 'assets/img/mf-logo.png'
+import AsidePanel from '@/components/AsidePanel'
+import SubTabPanel from '@/components/SubTabPanel'
+export default {
+	name: 'app',
+      data() {
+        return {
+          logo: logo
+        }
+      },
+    components: {
+        AsidePanel,
+        SubTabPanel
+    }
+}
+</script>
+
+<style>
+html, body, div#app, section.el-container{
+    height: 100%;
+}
+body{
+    margin: auto;
+    padding: 0;
+    font-family: "Hiragino Sans GB","Microsoft Yahei",SimSun,Arial,"Helvetica Neue",Helvetica;
+}
+.el-header,
+.el-footer {
+	background-color: #28353c;
+	color: #fff;
+	text-align: left;
+}
+
+.el-header > .logo-wrap{
+    margin-top: 8px;
+    width: 200px;
+}
+.el-header > .logo-wrap > span{
+    display: inline-block;
+    vertical-align: middle;
+}
+.el-header > .logo-wrap > span.logo-text{
+    margin-left: 10px;
+    font-size: 18px;
+}
+
+.el-aside {
+	background-color: #fff;
+	color: #333;
+	text-align: center;
+	line-height: 200px;
+}
+
+.el-main {
+	background-color: #E9ECF2;
+	color: #333;
+	text-align: center;
+    padding: 0;
+}
+
+body>.el-container {
+	margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+	line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+	line-height: 320px;
+}
+
+.subTabBar{
+    /*height: 40px;*/
+    background: #fff;
+}
+</style>
---
+++ src/components/AsidePanel.vue
@@ -0,0 +1,36 @@
+<template>
+<el-row class="tac">
+	<el-col :span="24">
+		<el-menu default-active="1-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#fff" text-color="#333" active-text-color="#00beff">
+			<el-submenu index="1">
+				<template slot="title"><span>邀约管理</span></template>
+				<el-menu-item-group>
+					<el-menu-item index="1-1">选项1</el-menu-item>
+					<el-menu-item index="1-2">选项2</el-menu-item>
+					<el-menu-item index="1-3">选项3</el-menu-item>
+				</el-menu-item-group>
+			</el-submenu>
+			<el-menu-item index="2">
+				<span slot="title">客户管理</span>
+			</el-menu-item>
+			<el-menu-item index="3">
+				<span slot="title">职位管理</span>
+			</el-menu-item>
+		</el-menu>
+	</el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+	name: 'aside',
+	methods: {
+		handleOpen(key, keyPath) {
+			console.log(key, keyPath);
+		},
+		handleClose(key, keyPath) {
+			console.log(key, keyPath);
+		}
+	}
+}
+</script>
